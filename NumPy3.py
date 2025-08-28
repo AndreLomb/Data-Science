1c8e566c02710385dfc709d2e164d1ae3c01d2d9
André <andrem6169@gmail.com>
2025-08-28 19:07:51 -0300

    lógica corrigida e respostas readequadas de acordo com o enunciado

--- NumPy3.py
+++ NumPy3.py
@@ -8,20 +8,24 @@ ds = np.loadtxt('space.csv',
 #Apresente a porcentagem de missões que deram certo 
 missions = ds[1:,7]
 
+total_missions = len(missions)
+
 missions_sucess = np.char.find(missions, 'Success') >= 0
 
-count_missions = missions_sucess.sum()
+mission_sucess_total = missions_sucess.sum()
 
-print(count_missions, 'missões deram certo\n')
+mission_mean = mission_sucess_total/total_missions
+
+print(f"{mission_mean*100:.3f}% de missões bem sucedidas\n")
 
 #Qual a media de gastos de uma missão espacial se baseando em missões que possuam valores disponíveis (>0)?
-mission_spending = ds[1:,6]
+mission_spending = ds[1:,6].astype(float)
 
-mission_spending_float = mission_spending.astype(float)
+valid_spending = mission_spending[mission_spending > 0]
 
-average_spending = mission_spending_float.mean()
+average_spending = valid_spending.mean()
 
-print('A média de gastos foi de:', average_spending, '\n')
+print(f"Houve um gasto médio de {average_spending:.3f} milhões de $\n")
 
 #Encontre quantas missões foram realizadas pelos Estados Unidos (EUA)
 american_mission = ds[1:, 2]
@@ -30,10 +34,28 @@ is_american_mission = np.char.find(american_mission, 'USA') >= 0
 
 number_missions = is_american_mission.sum()
 
-print(number_missions, 'missões feitas pelos EUA', '\n')
+print(number_missions, 'missões americanas', '\n')
 
 #Encontre a missão mais cara da SpaceX
+spacex_mission = ds[ds[:, 1] == 'SpaceX']
+spacex_mission_cost = ds[1:, 6]
 
+spacex_mission_cost_float = spacex_mission_cost.astype(float)
 
+max_spacex_mission = spacex_mission[np.argmax(spacex_mission[:, 6].astype(float))]
+
+spacex_name = max_spacex_mission[4]
+spacex_cost = max_spacex_mission[6].astype(float)
+
+print('A missão mais cara da SpaceX foi:\n', spacex_name, ';A qual custou ', spacex_cost, ' milhões de $\n')
 
 #Mostre o nome das empresas que já realizaram missões espaciais, juntamente com suas respectivas quantidades de missões (use o for no final para mostrar as informações)
+
+company_missions = ds[1:, 1]
+
+company_sucess = np.unique(company_missions)
+
+for missions_made in company_sucess:
+    join = company_missions == missions_made
+    num_missions = join.sum()
+    print(f"{missions_made} - {num_missions} missão(ões) da empresa")
